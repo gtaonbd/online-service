495059237aec3de49b85a7e8fbff1d9ffccdbb1e
gtaonbd <163102471+gtaonbd@users.noreply.github.com>
2024-04-02 11:37:56 +0600

    Update teamxshofikul.js

--- teamxshofikul.js
+++ teamxshofikul.js
@@ -167,7 +167,10 @@ text-align:center;line-height:35px;
 `);
 setDiv.setAttribute("id","setDiv");
 var svg=document.createElement("div");
+
 svg.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  
+  
 <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
 </svg>`;
 setDiv.appendChild(svg);
@@ -462,11 +465,16 @@ ytpSetI.innerHTML+=`<b style='font-size:18px' >TEAM X 1337</b>
 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg>1337</div>
 <br>
-<div style="font-size:13px;"><b style="font-weight:bold;color:cyan;">Features</b>:
-Discover a YouTube-like experience with all premium options available, completely ad-free.Re Developed By : <a href="#" onclick="Android.oplink('https://about-me-two-chi.vercel.app/')" >Shofikul Islam</a>
-<br><br><center>
-<a href="#" onclick="Android.oplink('https://t.me/S80F9K6L1337')" >Contact Developer</a>
-</center></div>`;
+
+<div style="font-size:13px;">
+  <b style="font-weight:bold; color:cyan; text-decoration:none">Features</b>:
+  Discover a YouTube-like experience with all premium options available, completely ad-free. 
+  Re-Developed By : <a href="#" onclick="Android.oplink('https://about-me-two-chi.vercel.app/')" style="color:blue;">Shofikul Islam</a>
+  <br><br>
+  <center>
+    <a href="#" onclick="Android.oplink('https://t.me/S80F9K6L1337')" style="color:blue; text-decoration:underline;">Contact Developer</a>
+  </center>
+</div>`;
 
 document.body.appendChild(ytpSet);
 ytpSet.appendChild(ytpSetI);
